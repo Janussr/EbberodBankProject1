3829367876c04aafb4cf6a225336cecab62b55ef
Julius <PsychedelicCoder@users.noreply.github.com>
2021-02-16 10:48:34 +0100

    Added a new Class MainController + added switch case function + some changes to our main class

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class MainController {
+import java.util.Scanner;
+import java.sql.*;
+
+
+public class MainController {
+
+    Scanner sc = new Scanner(System.in);
+    public void runProgram() {
+
+       int choice = 0;
+
+       while (choice != 9) {
+           choice = sc.nextInt();
+           switch (choice) {
+               case 1:
+                   placeHolder();
+                   break;
+               case 2:
+                   placeHolder();
+                   break;
+               case 3:
+                   placeHolder();
+                   break;
+               case 4:
+                   placeHolder();
+                   break;
+               case 5:
+                   placeHolder();
+                   break;
+               case 6:
+                   placeHolder();
+                   break;
+               case 0:
+                   exit();
+                   break;
+               default:
+                   exit();
+           }
+       }
+
+       //Functions to the related switch-case number will be added below here.
+
+
+
+
+    }
 }
